c177930d250fe64c76995cd61af3faaf346c0d7a
Iurii Polshchikov <polurival@gmail.com>
2016-02-07 23:40:29 +0300

    right solution for level25.lesson16.big01 task-4

--- src/com/javarush/test/level25/lesson16/big01/Space.java
+++ src/com/javarush/test/level25/lesson16/big01/Space.java
@@ -1,11 +1,66 @@
 package com.javarush.test.level25.lesson16.big01;
 
+import java.util.ArrayList;
+
 /**
  * Created by Polurival
  * on 07.02.2016.
  */
 public class Space
 {
+    private int height;
+    private int width;
+
+    private SpaceShip ship;
+
+    private ArrayList<Ufo> ufos = new ArrayList<Ufo>();
+    private ArrayList<Rocket> rockets = new ArrayList<Rocket>();
+    private ArrayList<Bomb> bombs = new ArrayList<Bomb>();
+
+
+    public Space(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
+
+
+    public int getHeight()
+    {
+        return height;
+    }
+
+    public int getWidth()
+    {
+        return width;
+    }
+
+    public SpaceShip getShip()
+    {
+        return ship;
+    }
+
+    public ArrayList<Ufo> getUfos()
+    {
+        return ufos;
+    }
+
+    public ArrayList<Rocket> getRockets()
+    {
+        return rockets;
+    }
+
+    public ArrayList<Bomb> getBombs()
+    {
+        return bombs;
+    }
+
+
+    public void setShip(SpaceShip ship)
+    {
+        this.ship = ship;
+    }
+
+
     public static void main(String[] args)
     {
 
