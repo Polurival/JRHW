853a77d474a543420d0639dcbdd5dd96ce74285b
Iurii Polshchikov <polurival@gmail.com>
2016-02-07 16:09:53 +0300

    right solution for level25.lesson11.task02

---
+++ src/com/javarush/test/level25/lesson11/task02/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.test.level25.lesson11.task02;
+
+/* Первый закон Финэйгла: если эксперимент удался, что-то здесь не так...
+Обеспечьте переуступку кванта времени (переход хода для текущей нити) для последовательных выводов текста в консоль
+*/
+public class Solution {
+    public static class YieldRunnable implements Runnable {
+        private int index;
+
+        public YieldRunnable(int index) {
+            this.index = index;
+        }
+
+        public void run() {
+            System.out.println("begin-" + index);
+            Thread.yield();
+            System.out.println("end-" + index);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Solution.YieldRunnable sy = new Solution.YieldRunnable(5);
+        Thread thread = new Thread(sy);
+        thread.start();
+    }
+}
