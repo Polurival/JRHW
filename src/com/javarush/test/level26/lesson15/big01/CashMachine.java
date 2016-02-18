83ce1c806d6bf737ddecaa3082cd0f8cd02c6e0a
Iurii Polshchikov <polurival@gmail.com>
2016-02-18 22:37:52 +0300

    right solution for level26.lesson15.big01 task01

---
+++ src/com/javarush/test/level26/lesson15/big01/CashMachine.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level26.lesson15.big01;
+
+/**
+ * Created by Polurival
+ * on 18.02.2016.
+ */
+public class CashMachine
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
