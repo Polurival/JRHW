b7d393aa30aa456a3388bd1af1a7de6a7eb65e77
Iurii Polshchikov <polurival@gmail.com>
2016-02-18 22:57:08 +0300

    right solution for level26.lesson15.big01 task02

--- src/com/javarush/test/level26/lesson15/big01/ConsoleHelper.java
+++ src/com/javarush/test/level26/lesson15/big01/ConsoleHelper.java
@@ -1,9 +1,27 @@
 package com.javarush.test.level26.lesson15.big01;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * Created by Polurival
  * on 18.02.2016.
  */
 public class ConsoleHelper
 {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        String message = null;
+        try {
+            message = reader.readLine();
+        } catch (Exception e) {
+
+        }
+        return message;
+    }
 }
---
+++ src/com/javarush/test/level26/lesson15/big01/exception/InterruptOperationException.java
@@ -0,0 +1,9 @@
+package com.javarush.test.level26.lesson15.big01.exception;
+
+/**
+ * Created by Polurival
+ * on 18.02.2016.
+ */
+public class InterruptOperationException extends Exception
+{
+}
