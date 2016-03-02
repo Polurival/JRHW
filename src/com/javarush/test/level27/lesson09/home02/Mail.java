89841c72c95634444cc14733a4e9ac799985dee4
Polurival <polurival@gmail.com>
2016-03-02 23:01:06 +0300

    right solution for level27.lesson09.home02 with refactoring

--- src/com/javarush/test/level27/lesson09/home02/Mail.java
+++ src/com/javarush/test/level27/lesson09/home02/Mail.java
@@ -3,11 +3,11 @@ package com.javarush.test.level27.lesson09.home02;
 public class Mail {
     private String text;
 
-    public synchronized String getText() {
+    public String getText() {
         return text;
     }
 
-    public synchronized void setText(String text) {
+    public void setText(String text) {
         this.text = text;
     }
 }
