1cf7bd08fac6d8b179d9e4949e60be558c5ddee7
Polurival <polurival@gmail.com>
2016-03-05 15:09:34 +0300

    right solution for level27.lesson15.big01 task07

--- src/com/javarush/test/level27/lesson15/big01/ad/AdvertisementStorage.java
+++ src/com/javarush/test/level27/lesson15/big01/ad/AdvertisementStorage.java
@@ -1,18 +1,38 @@
 package com.javarush.test.level27.lesson15.big01.ad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by
  * Polurival on 05.03.2016.
  */
-public class AdvertisementStorage
+class AdvertisementStorage
 {
     private static AdvertisementStorage instance = new AdvertisementStorage();
+
+    private final List<Object> videos = new ArrayList<>();
+
+    private AdvertisementStorage()
+    {
+        Object someContent = new Object();
+        videos.add(new Advertisement(someContent, "First Video", 5000, 100, 3 * 60));
+        videos.add(new Advertisement(someContent, "Second Video", 100, 10, 15 * 60));
+        videos.add(new Advertisement(someContent, "Third Video", 400, 2, 10 * 60));
+    }
 
     public static AdvertisementStorage getInstance()
     {
         return instance;
     }
 
-    private AdvertisementStorage() {
+    public List<Object> list()
+    {
+        return videos;
+    }
+
+    public void add(Advertisement advertisement)
+    {
+        videos.add(advertisement);
     }
 }
