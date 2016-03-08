2279fde89b559b43b67b1be41000c09f0141b984
Polurival <polurival@gmail.com>
2016-03-08 18:35:11 +0300

    right solution for level27.lesson15.big01 task13

--- src/com/javarush/test/level27/lesson15/big01/statistic/StatisticManager.java
+++ src/com/javarush/test/level27/lesson15/big01/statistic/StatisticManager.java
@@ -1,6 +1,12 @@
 package com.javarush.test.level27.lesson15.big01.statistic;
 
 import com.javarush.test.level27.lesson15.big01.statistic.event.EventDataRow;
+import com.javarush.test.level27.lesson15.big01.statistic.event.EventType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by
@@ -8,6 +14,18 @@ import com.javarush.test.level27.lesson15.big01.statistic.event.EventDataRow;
  */
 public class StatisticManager
 {
+    private class StatisticStorage {
+        private Map<EventType, List<EventDataRow>> map = new HashMap<>();
+
+        public StatisticStorage() {
+            map.put(EventType.COOKED_ORDER, new ArrayList<EventDataRow>());
+            map.put(EventType.SELECTED_VIDEOS, new ArrayList<EventDataRow>());
+            map.put(EventType.NO_AVAILABLE_VIDEO, new ArrayList<EventDataRow>());
+        }
+    }
+
+    private StatisticStorage statisticStorage = new StatisticStorage();
+
     private static StatisticManager instance = new StatisticManager();
 
     private StatisticManager() {
