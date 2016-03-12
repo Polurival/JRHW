99af78e2afc868949405df664548e7c3a5262f86
Polurival <polurival@gmail.com>
2016-03-12 22:11:25 +0300

    first clause  for level27.lesson15.big01 task23

--- src/com/javarush/test/level27/lesson15/big01/kitchen/TestOrder.java
+++ src/com/javarush/test/level27/lesson15/big01/kitchen/TestOrder.java
@@ -4,7 +4,7 @@ import com.javarush.test.level27.lesson15.big01.Tablet;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Created by
@@ -21,7 +21,7 @@ public class TestOrder extends Order
     protected void initDishes()
     {
         dishes = new ArrayList<>();
-        Random random = new Random();
+        ThreadLocalRandom random = ThreadLocalRandom.current();
         int i = random.nextInt(Dish.values().length);
         int j = random.nextInt(Dish.values().length);
         int k = random.nextInt(Dish.values().length);
