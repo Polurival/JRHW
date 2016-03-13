80fe735cb24826d81fd0ad8efd187b4405e5b46c
Polurival <polurival@gmail.com>
2016-03-13 22:58:01 +0300

    right solution for level28.lesson15.big01 task02

--- src/com/javarush/test/level28/lesson15/big01/Aggregator.java
+++ src/com/javarush/test/level28/lesson15/big01/Aggregator.java
@@ -1,5 +1,8 @@
 package com.javarush.test.level28.lesson15.big01;
 
+import com.javarush.test.level28.lesson15.big01.model.Provider;
+import com.javarush.test.level28.lesson15.big01.model.Strategy;
+
 /**
  * Created by
  * Polurival on 13.03.2016.
@@ -8,6 +11,10 @@ public class Aggregator
 {
     public static void main(String[] args)
     {
-
+        Provider provider = new Provider(new Strategy()
+        {
+        });
+        Controller controller = new Controller(provider);
+        System.out.println(controller);
     }
 }
