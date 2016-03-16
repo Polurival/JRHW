390185f0795d52c1ac9e2f9075ac7c2ca876a3a3
Polurival <polurival@gmail.com>
2016-03-16 23:54:01 +0300

    right solution for level28.lesson15.big01 task08

--- src/com/javarush/test/level28/lesson15/big01/model/HHStrategy.java
+++ src/com/javarush/test/level28/lesson15/big01/model/HHStrategy.java
@@ -3,9 +3,12 @@ package com.javarush.test.level28.lesson15.big01.model;
 import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -19,9 +22,49 @@ public class HHStrategy implements Strategy
     @Override
     public List<Vacancy> getVacancies(String searchString) throws IOException
     {
+        List<Vacancy> vacancies = new ArrayList<>();
+
+        for (int i = 0; ; i++)
+        {
+            Document document = getDocument(searchString, i);
+            Elements plainVacancies = document.select("[data-qa=\"vacancy-serp__vacancy\"]");
+            if (plainVacancies.size() == 0)
+            {
+                break;
+            }
+            Iterator iterator = plainVacancies.iterator();
+            Element rawVacancy;
+            while (iterator.hasNext())
+            {
+                Vacancy vacancy = new Vacancy();
+                rawVacancy = (Element) iterator.next();
+
+                vacancy.setTitle(rawVacancy.select("[data-qa=\"vacancy-serp__vacancy-title\"]").first().text());
+
+                Elements salaries = rawVacancy.select("[data-qa=\"vacancy-serp__vacancy-compensation\"]");
+                if (salaries.size() > 0)
+                {
+                    vacancy.setSalary(salaries.first().text());
+                } else
+                {
+                    vacancy.setSalary("");
+                }
+
+                vacancy.setCity(rawVacancy.select("[data-qa=\"vacancy-serp__vacancy-address\"]").first().text());
+                vacancy.setCompanyName(rawVacancy.select("[data-qa=\"vacancy-serp__vacancy-employer\"]").first().text());
+                vacancy.setSiteName("hh.ru");
+                vacancy.setUrl(rawVacancy.select("[data-qa=\"vacancy-serp__vacancy-title\"]").first().attr("href"));
+
+                vacancies.add(vacancy);
+            }
+        }
+        return vacancies;
+    }
+
+    protected Document getDocument(String searchString, int page) throws IOException
+    {
         String userAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36";
-        String referrer = "https://hh.ua/search/vacancy?text=java+Киев";
-        Document document = Jsoup.connect(String.format(URL_FORMAT, "Киев", 1)).userAgent(userAgent).referrer(referrer).get();
-        return new ArrayList<>();
+        String referrer = "google.ru";
+        return Jsoup.connect(searchString).userAgent(userAgent).referrer(referrer).get();
     }
 }
--- src/com/javarush/test/level28/lesson15/big01/model/Provider.java
+++ src/com/javarush/test/level28/lesson15/big01/model/Provider.java
@@ -3,7 +3,6 @@ package com.javarush.test.level28.lesson15.big01.model;
 import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
