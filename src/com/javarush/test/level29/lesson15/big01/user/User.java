58174d72e38773475021ac08c9898d7ef3e5384e
Polurival <polurival@gmail.com>
2016-03-20 00:49:07 +0300

    right solution for level29.lesson15.big01 task13

--- src/com/javarush/test/level29/lesson15/big01/user/User.java
+++ src/com/javarush/test/level29/lesson15/big01/user/User.java
@@ -1,6 +1,7 @@
 package com.javarush.test.level29.lesson15.big01.user;
 
-public class User {
+public class User
+{
     private String name;
     private String surname;
     private int age;
@@ -11,61 +12,89 @@ public class User {
 
     private Work work;
 
-    public User(String name, String surname, int age) {
+    public User(String name, String surname, int age)
+    {
         this.name = name;
         this.surname = surname;
         this.age = age;
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name)
+    {
         this.name = name;
     }
 
-    public String getSurname() {
+    public String getSurname()
+    {
         return surname;
     }
 
-    public void setSurname(String surname) {
+    public void setSurname(String surname)
+    {
         this.surname = surname;
     }
 
-    public int getAge() {
+    public int getAge()
+    {
         return age;
     }
 
-    public void setAge(int age) {
+    public void setAge(int age)
+    {
         this.age = age;
     }
 
-    public String getCountry() {
+    public String getCountry()
+    {
         return country;
     }
 
-    public void setCountry(String country) {
+    public void setCountry(String country)
+    {
         this.country = country;
     }
 
-    public String getCity() {
+    public String getCity()
+    {
         return city;
     }
 
-    public void setCity(String city) {
+    public void setCity(String city)
+    {
         this.city = city;
     }
 
-    public String getAddress() {
+    public String getAddress()
+    {
         return country + " " + city + " " + house.house;
     }
 
-    public Work getWork() {
+    public Work getWork()
+    {
         return work;
     }
 
-    public void setWork(Work work) {
+    public void setWork(Work work)
+    {
         this.work = work;
+    }
+
+    public void printInfo()
+    {
+        System.out.println("Имя: " + this.getName());
+        System.out.println("Фамилия: " + this.getSurname());
+    }
+
+    public void printAdditionalInfo()
+    {
+        if (this.getAge() < 16)
+            System.out.println("Пользователь моложе 16 лет");
+        else
+            System.out.println("Пользователь старше 16 лет");
     }
 }
--- src/com/javarush/test/level29/lesson15/big01/user/Work.java
+++ src/com/javarush/test/level29/lesson15/big01/user/Work.java
@@ -1,13 +1,16 @@
 package com.javarush.test.level29.lesson15.big01.user;
 
-public class Work {
+public class Work
+{
     private String boss;
 
-    public String getBoss() {
+    public String getBoss()
+    {
         return boss;
     }
 
-    public void setBoss(String boss) {
+    public void setBoss(String boss)
+    {
         this.boss = boss;
     }
 }
