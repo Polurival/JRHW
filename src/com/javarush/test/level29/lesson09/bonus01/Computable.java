4596ded814e0c00fa0f170ca78fe061aab2f8ae8
Polurival <polurival@gmail.com>
2016-03-19 14:00:23 +0300

    right solution for level29.lesson09.bonus01

---
+++ src/com/javarush/test/level29/lesson09/bonus01/Computable.java
@@ -0,0 +1,7 @@
+package com.javarush.test.level29.lesson09.bonus01;
+
+/* Argument and Value are generic types*/
+public interface Computable<Argument, Value>
+{
+    Value compute(Argument argument) throws InterruptedException;
+}
