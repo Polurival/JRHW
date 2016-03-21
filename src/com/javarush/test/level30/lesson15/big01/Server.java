dbad67a53b2f39be8ca9ec1e3bb9976d5c2e8e33
Polurival <polurival@gmail.com>
2016-03-21 23:48:49 +0300

    right solution for level30.lesson15.big01 task06

--- src/com/javarush/test/level30/lesson15/big01/Server.java
+++ src/com/javarush/test/level30/lesson15/big01/Server.java
@@ -1,9 +1,42 @@
 package com.javarush.test.level30.lesson15.big01;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by
  * Polurival on 21.03.2016.
  */
 public class Server
 {
+    private static class Handler extends Thread
+    {
+        private Socket socket;
+
+        public Handler(Socket socket)
+        {
+            this.socket = socket;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        int port = Integer.valueOf(ConsoleHelper.readString());
+        try (ServerSocket serverSocket = new ServerSocket(port))
+        {
+            ConsoleHelper.writeMessage("Server is running");
+            while (true)
+            {
+                Socket socket = serverSocket.accept();
+                Handler handler = new Handler(socket);
+                handler.start();
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
