5d1014cba6b05eaf76c9be5074932d13d896f47d
Polurival <polurival@gmail.com>
2016-03-21 22:16:21 +0300

    right solution for level30.lesson15.big01 task04

--- src/com/javarush/test/level30/lesson15/big01/Message.java
+++ src/com/javarush/test/level30/lesson15/big01/Message.java
@@ -1,9 +1,35 @@
 package com.javarush.test.level30.lesson15.big01;
 
+import java.io.Serializable;
+
 /**
  * Created by
  * Polurival on 21.03.2016.
  */
-public class Message
+public class Message implements Serializable
 {
+    private final MessageType type;
+
+    private final String data;
+
+    public MessageType getType()
+    {
+        return type;
+    }
+
+    public String getData()
+    {
+        return data;
+    }
+
+    public Message(MessageType type, String data)
+    {
+        this.type = type;
+        this.data = data;
+    }
+
+    public Message(MessageType type)
+    {
+        this(type, null);
+    }
 }
