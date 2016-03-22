cf8dda4ae8e285cfde0cf7808b9d008421d5afa5
Polurival <polurival@gmail.com>
2016-03-22 20:55:49 +0300

    right solution for level30.lesson15.big01 task12

---
+++ src/com/javarush/test/level30/lesson15/big01/client/Client.java
@@ -0,0 +1,18 @@
+package com.javarush.test.level30.lesson15.big01.client;
+
+import com.javarush.test.level30.lesson15.big01.Connection;
+
+/**
+ * Created by
+ * Polurival on 22.03.2016.
+ */
+public class Client
+{
+    protected Connection connection;
+    private volatile boolean clientConnected = false;
+
+    public class SocketThread extends Thread
+    {
+
+    }
+}
