14336db0625073dbb6396e3253a067506ced5967
Polurival <polurival@gmail.com>
2016-04-02 18:23:31 +0300

    right solution for level32.lesson15.big01 task11

--- src/com/javarush/test/level32/lesson15/big01/View.java
+++ src/com/javarush/test/level32/lesson15/big01/View.java
@@ -2,8 +2,12 @@ package com.javarush.test.level32.lesson15.big01;
 
 import com.javarush.test.level32.lesson15.big01.listeners.FrameListener;
 import com.javarush.test.level32.lesson15.big01.listeners.TabbedPaneChangeListener;
+import com.javarush.test.level32.lesson15.big01.listeners.UndoListener;
 
 import javax.swing.*;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+import javax.swing.undo.UndoManager;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,11 +19,15 @@ import java.awt.event.ActionListener;
 public class View extends JFrame implements ActionListener
 {
     private Controller controller;
+
     private FrameListener frameListener;
 
     private JTabbedPane tabbedPane = new JTabbedPane();
     private JTextPane htmlTextPane = new JTextPane();
     private JEditorPane plainTextPane = new JEditorPane();
+
+    private UndoManager undoManager = new UndoManager();
+    private UndoListener undoListener = new UndoListener(undoManager);
 
     public Controller getController()
     {
@@ -29,6 +37,11 @@ public class View extends JFrame implements ActionListener
     public void setController(Controller controller)
     {
         this.controller = controller;
+    }
+
+    public UndoListener getUndoListener()
+    {
+        return undoListener;
     }
 
     public View()
@@ -109,11 +122,40 @@ public class View extends JFrame implements ActionListener
 
     public boolean canUndo()
     {
-        return false;
+        return undoManager.canUndo();
     }
 
     public boolean canRedo()
     {
-        return false;
+        return undoManager.canRedo();
+    }
+
+    public void undo()
+    {
+        try
+        {
+            undoManager.undo();
+        }
+        catch (CannotUndoException e)
+        {
+            ExceptionHandler.log(e);
+        }
+    }
+
+    public void redo()
+    {
+        try
+        {
+            undoManager.redo();
+        }
+        catch (CannotRedoException e)
+        {
+            ExceptionHandler.log(e);
+        }
+    }
+
+    public void resetUndo()
+    {
+        undoManager.discardAllEdits();
     }
 }
---
+++ src/com/javarush/test/level32/lesson15/big01/listeners/UndoListener.java
@@ -0,0 +1,25 @@
+package com.javarush.test.level32.lesson15.big01.listeners;
+
+import javax.swing.event.UndoableEditEvent;
+import javax.swing.event.UndoableEditListener;
+import javax.swing.undo.UndoManager;
+
+/**
+ * Created by
+ * Polurival on 02.04.2016.
+ */
+public class UndoListener implements UndoableEditListener
+{
+    UndoManager undoManager;
+
+    public UndoListener(UndoManager undoManager)
+    {
+        this.undoManager = undoManager;
+    }
+
+    @Override
+    public void undoableEditHappened(UndoableEditEvent e)
+    {
+        undoManager.addEdit(e.getEdit());
+    }
+}
