e55bc0f1719c5d26a7c6588300a5ed0dc5b6b2e7
Polurival <polurival@gmail.com>
2016-04-02 14:39:57 +0300

    right solution for level32.lesson15.big01 task03

---
+++ src/com/javarush/test/level32/lesson15/big01/listeners/FrameListener.java
@@ -0,0 +1,26 @@
+package com.javarush.test.level32.lesson15.big01.listeners;
+
+import com.javarush.test.level32.lesson15.big01.View;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * Created by
+ * Polurival on 02.04.2016.
+ */
+public class FrameListener extends WindowAdapter
+{
+    private View view;
+
+    public FrameListener(View view)
+    {
+        this.view = view;
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e)
+    {
+        view.exit();
+    }
+}
