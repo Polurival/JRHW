b4390dac58b3cce0e59c84e1862dbb607669117b
Polurival <polurival@gmail.com>
2016-04-02 15:02:23 +0300

    right solution for level32.lesson15.big01 task05

---
+++ src/com/javarush/test/level32/lesson15/big01/ExceptionHandler.java
@@ -0,0 +1,12 @@
+package com.javarush.test.level32.lesson15.big01;
+
+/**
+ * Created by
+ * Polurival on 02.04.2016.
+ */
+public class ExceptionHandler
+{
+    public static void log(Exception e) {
+        System.out.println(e.toString());
+    }
+}
--- src/com/javarush/test/level32/lesson15/big01/View.java
+++ src/com/javarush/test/level32/lesson15/big01/View.java
@@ -60,4 +60,9 @@ public class View extends JFrame implements ActionListener
     public void exit() {
         controller.exit();
     }
+
+    public void selectedTabChanged()
+    {
+
+    }
 }
