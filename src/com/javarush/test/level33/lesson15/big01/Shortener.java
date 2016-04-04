704880df317dfa6857a403eaf555bdd0fe44311a
Polurival <polurival@gmail.com>
2016-04-04 22:57:17 +0300

    right solution for level33.lesson15.big01 task01

---
+++ src/com/javarush/test/level33/lesson15/big01/Shortener.java
@@ -0,0 +1,18 @@
+package com.javarush.test.level33.lesson15.big01;
+
+/**
+ * Created by
+ * Polurival on 04.04.2016.
+ */
+public class Shortener
+{
+    public Long getId(String string)
+    {
+        return null;
+    }
+
+    public String getString(Long id)
+    {
+        return null;
+    }
+}
