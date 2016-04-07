877f02aaef191301a405adcfb4d297f6e36ca472
Polurival <polurival@gmail.com>
2016-04-07 20:57:06 +0300

    right solution for level33.lesson15.big01 task11

--- src/com/javarush/test/level33/lesson15/big01/Solution.java
+++ src/com/javarush/test/level33/lesson15/big01/Solution.java
@@ -1,9 +1,6 @@
 package com.javarush.test.level33.lesson15.big01;
 
-import com.javarush.test.level33.lesson15.big01.strategies.FileStorageStrategy;
-import com.javarush.test.level33.lesson15.big01.strategies.HashMapStorageStrategy;
-import com.javarush.test.level33.lesson15.big01.strategies.OurHashMapStorageStrategy;
-import com.javarush.test.level33.lesson15.big01.strategies.StorageStrategy;
+import com.javarush.test.level33.lesson15.big01.strategies.*;
 
 import java.util.Date;
 import java.util.HashSet;
@@ -67,8 +64,9 @@ public class Solution
 
     public static void main(String[] args)
     {
-        testStrategy(new HashMapStorageStrategy(), 100);
-        testStrategy(new OurHashMapStorageStrategy(), 100);
-        testStrategy(new FileStorageStrategy(), 100);
+        testStrategy(new HashMapStorageStrategy(), 500);
+        testStrategy(new OurHashMapStorageStrategy(), 500);
+        testStrategy(new FileStorageStrategy(), 500);
+        testStrategy(new OurHashBiMapStorageStrategy(), 500);
     }
 }
