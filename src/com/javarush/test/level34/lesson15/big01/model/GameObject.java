2c5d9805e77aeaaead812c7d715eb2dac2dc7cb4
Yury Polshchikov <polurival@gmail.com>
2016-04-18 18:57:23 +0430

    right solution for level34.lesson15.big01 task01

---
+++ src/com/javarush/test/level34/lesson15/big01/model/GameObject.java
@@ -0,0 +1,70 @@
+package com.javarush.test.level34.lesson15.big01.model;
+
+import java.awt.*;
+
+/**
+ * Created by Polurival
+ * on 18.04.16.
+ */
+public abstract class GameObject
+{
+    private int x, y, width, height;
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+
+    public int getWidth()
+    {
+        return width;
+    }
+
+    public void setWidth(int width)
+    {
+        this.width = width;
+    }
+
+    public int getHeight()
+    {
+        return height;
+    }
+
+    public void setHeight(int height)
+    {
+        this.height = height;
+    }
+
+    public GameObject(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+        this.width = Model.FIELD_SELL_SIZE;
+        this.height = Model.FIELD_SELL_SIZE;
+    }
+
+    public GameObject(int x, int y, int width, int height)
+    {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public abstract void draw(Graphics graphics);
+}
--- src/com/javarush/test/level34/lesson15/big01/model/Model.java
+++ src/com/javarush/test/level34/lesson15/big01/model/Model.java
@@ -6,4 +6,5 @@ package com.javarush.test.level34.lesson15.big01.model;
  */
 public class Model
 {
+    public static int FIELD_SELL_SIZE = 20;
 }
