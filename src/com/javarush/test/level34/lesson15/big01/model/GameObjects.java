37d95fa83b1da2f9265c5220277f53e4627ba629
Yury Polshchikov <polurival@gmail.com>
2016-04-18 22:24:49 +0430

    right solution for level34.lesson15.big01 Sokoban task08

---
+++ src/com/javarush/test/level34/lesson15/big01/model/GameObjects.java
@@ -0,0 +1,53 @@
+package com.javarush.test.level34.lesson15.big01.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Polurival
+ * on 18.04.16.
+ */
+public class GameObjects
+{
+    private Set<Wall> walls;
+    private Set<Box> boxes;
+    private Set<Home> homes;
+    private Player player;
+
+    public Set<Wall> getWalls()
+    {
+        return walls;
+    }
+
+    public Set<Box> getBoxes()
+    {
+        return boxes;
+    }
+
+    public Set<Home> getHomes()
+    {
+        return homes;
+    }
+
+    public Player getPlayer()
+    {
+        return player;
+    }
+
+    public GameObjects(Set<Wall> walls, Set<Box> boxes, Set<Home> homes, Player player)
+    {
+        this.walls = walls;
+        this.boxes = boxes;
+        this.homes = homes;
+        this.player = player;
+    }
+
+    public Set<GameObject> getAll() {
+        Set<GameObject> allObjects = new HashSet<>();
+        allObjects.addAll(this.getWalls());
+        allObjects.addAll(this.getBoxes());
+        allObjects.addAll(this.getHomes());
+        allObjects.add(this.getPlayer());
+        return allObjects;
+    }
+}
