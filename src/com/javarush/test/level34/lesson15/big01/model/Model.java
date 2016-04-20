a9ed9c1f911ca3e0ee781ec4c30ee7c52f0243b0
Yury Polshchikov <polurival@gmail.com>
2016-04-20 21:10:37 +0430

    right solution for level34.lesson15.big01 Sokoban task15

--- src/com/javarush/test/level34/lesson15/big01/model/Model.java
+++ src/com/javarush/test/level34/lesson15/big01/model/Model.java
@@ -3,6 +3,7 @@ package com.javarush.test.level34.lesson15.big01.model;
 import com.javarush.test.level34.lesson15.big01.controller.EventListener;
 
 import java.nio.file.Paths;
+import java.util.Set;
 
 /**
  * Created by Polurival
@@ -50,6 +51,117 @@ public class Model
 
     public void move(Direction direction)
     {
+        Player player = gameObjects.getPlayer();
 
+        if (checkWallCollision(player, direction))
+        {
+            return;
+        }
+        if (checkBoxCollision(direction))
+        {
+            return;
+        }
+
+        int newX = 0;
+        int newY = 0;
+        switch (direction)
+        {
+            case UP:
+                newY -= FIELD_SELL_SIZE;
+                break;
+            case DOWN:
+                newY += FIELD_SELL_SIZE;
+                break;
+            case LEFT:
+                newX -= FIELD_SELL_SIZE;
+                break;
+            case RIGHT:
+                newX += FIELD_SELL_SIZE;
+                break;
+        }
+
+        player.move(newX, newY);
+
+        checkCompletion();
+    }
+
+    public boolean checkWallCollision(CollisionObject gameObject, Direction direction)
+    {
+        for (Wall wall : gameObjects.getWalls())
+        {
+            if (gameObject.isCollision(wall, direction))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkBoxCollision(Direction direction)
+    {
+        Player player = gameObjects.getPlayer();
+        Set<Box> boxes = gameObjects.getBoxes();
+
+        for (Box box : boxes)
+        {
+            if (player.isCollision(box, direction))
+            {
+                for (Box secondBox : boxes)
+                {
+                    if (!(box.getX() == secondBox.getX() && box.getY() == secondBox.getY()))
+                    {
+                        if (box.isCollision(secondBox, direction))
+                        {
+                            return true;
+                        }
+                    }
+                }
+
+                if (checkWallCollision(box, direction))
+                {
+                    return true;
+                }
+
+                int newX = 0;
+                int newY = 0;
+                switch (direction)
+                {
+                    case UP:
+                        newY -= FIELD_SELL_SIZE;
+                        break;
+                    case DOWN:
+                        newY += FIELD_SELL_SIZE;
+                        break;
+                    case LEFT:
+                        newX -= FIELD_SELL_SIZE;
+                        break;
+                    case RIGHT:
+                        newX += FIELD_SELL_SIZE;
+                        break;
+                }
+                box.move(newX, newY);
+            }
+        }
+        return false;
+    }
+
+    public void checkCompletion()
+    {
+        int count = 0;
+        for (Home home : gameObjects.getHomes())
+        {
+            for (Box box : gameObjects.getBoxes())
+            {
+                if (home.getX() == box.getX() && home.getY() == box.getY())
+                {
+                    count++;
+                    break;
+                }
+            }
+        }
+        if (count == gameObjects.getHomes().size())
+        {
+            eventListener.levelCompleted(currentLevel);
+        }
     }
 }
