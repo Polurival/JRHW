12785ac6882786aa4d96bcac6488a73c5c57beb0
Yury Polshchikov <polurival@gmail.com>
2016-04-20 19:51:03 +0430

    right solution for level34.lesson15.big01 Sokoban task14

--- src/com/javarush/test/level34/lesson15/big01/view/Field.java
+++ src/com/javarush/test/level34/lesson15/big01/view/Field.java
@@ -1,11 +1,14 @@
 package com.javarush.test.level34.lesson15.big01.view;
 
 import com.javarush.test.level34.lesson15.big01.controller.EventListener;
+import com.javarush.test.level34.lesson15.big01.model.Direction;
 import com.javarush.test.level34.lesson15.big01.model.GameObject;
 import com.javarush.test.level34.lesson15.big01.model.GameObjects;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 /**
  * Created by Polurival
@@ -24,6 +27,9 @@ public class Field extends JPanel
     public Field(View view)
     {
         this.view = view;
+        KeyHandler keyHandler = new KeyHandler();
+        addKeyListener(keyHandler);
+        setFocusable(true);
     }
 
     @Override
@@ -35,6 +41,32 @@ public class Field extends JPanel
         for (GameObject gameObject : gameObjects.getAll())
         {
             gameObject.draw(g);
+        }
+    }
+
+    public class KeyHandler extends KeyAdapter
+    {
+        @Override
+        public void keyPressed(KeyEvent e)
+        {
+            switch (e.getKeyCode())
+            {
+                case KeyEvent.VK_LEFT:
+                    eventListener.move(Direction.LEFT);
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    eventListener.move(Direction.RIGHT);
+                    break;
+                case KeyEvent.VK_DOWN:
+                    eventListener.move(Direction.DOWN);
+                    break;
+                case KeyEvent.VK_UP:
+                    eventListener.move(Direction.UP);
+                    break;
+                case KeyEvent.VK_R:
+                    eventListener.restart();
+                    break;
+            }
         }
     }
 }
