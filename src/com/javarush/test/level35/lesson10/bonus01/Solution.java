22a731140b60cfd85f5568022aebe671f06f6853
Yury Polshchikov <polurival@gmail.com>
2016-04-22 23:35:03 +0430

    right solution for level35.lesson10.bonus01 ClassLoader

--- src/com/javarush/test/level35/lesson10/bonus01/Solution.java
+++ src/com/javarush/test/level35/lesson10/bonus01/Solution.java
@@ -1,7 +1,8 @@
 package com.javarush.test.level35.lesson10.bonus01;
 
-import java.io.File;
+import java.io.*;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -27,6 +28,11 @@ public class Solution
     {
         Set<Animal> set = new HashSet<>();
 
+        if (!pathToAnimals.endsWith("\\") && !pathToAnimals.endsWith("/"))
+        {
+            pathToAnimals = pathToAnimals + "/";
+        }
+
         String[] files = new File(pathToAnimals).list();
 
         for (String file : files)
@@ -36,10 +42,30 @@ public class Solution
 
             try
             {
-                ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-                String clazzName = file.split(".class")[0];
-                String packaje = Animal.class.getPackage().getName();
-                Class clazz = classLoader.loadClass(packaje + ".data." + clazzName);
+                final String finalPathToAnimals = pathToAnimals;
+                ClassLoader loader = new ClassLoader()
+                {
+                    @Override
+                    public Class<?> findClass(String className) throws ClassNotFoundException
+                    {
+                        try
+                        {
+                            byte b[] = fetchClassFromFS(finalPathToAnimals + className + ".class");
+                            return defineClass(null, b, 0, b.length);
+                        }
+                        catch (FileNotFoundException ex)
+                        {
+                            return super.findClass(className);
+                        }
+                        catch (IOException ex)
+                        {
+                            return super.findClass(className);
+                        }
+                    }
+                };
+
+                String fileName = file.substring(0, file.length()-6);
+                Class clazz = loader.loadClass(fileName);
 
                 for (Class interfaze : clazz.getInterfaces())
                 {
@@ -52,7 +78,7 @@ public class Solution
 
                 for (Constructor constructor : clazz.getConstructors())
                 {
-                    if (constructor.getParameterTypes().length == 0)
+                    if (Modifier.isPublic(constructor.getModifiers()) && constructor.getParameterTypes().length == 0)
                     {
                         hasDefaultConstructor = true;
                         break;
@@ -62,8 +88,7 @@ public class Solution
 
                 if (hasAnimalInterface && hasDefaultConstructor)
                 {
-                    Animal animal = (Animal) clazz.newInstance();
-                    set.add(animal);
+                    set.add((Animal) clazz.newInstance());
                 }
             }
             catch (ClassNotFoundException | InstantiationException | IllegalAccessException e)
@@ -73,5 +98,34 @@ public class Solution
         }
 
         return set;
+    }
+
+    private static byte[] fetchClassFromFS(String path) throws FileNotFoundException, IOException
+    {
+        InputStream is = new FileInputStream(new File(path));
+        // Get the size of the file
+        long length = new File(path).length();
+        if (length > Integer.MAX_VALUE)
+        {
+            // File is too large
+        }
+        // Create the byte array to hold the data
+        byte[] bytes = new byte[(int) length];
+        // Read in the bytes
+        int offset = 0;
+        int numRead = 0;
+        while (offset < bytes.length
+                && (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0)
+        {
+            offset += numRead;
+        }
+        // Ensure all the bytes have been read in
+        if (offset < bytes.length)
+        {
+            throw new IOException("Could not completely read file " + path);
+        }
+        // Close the input stream and return bytes
+        is.close();
+        return bytes;
     }
 }
