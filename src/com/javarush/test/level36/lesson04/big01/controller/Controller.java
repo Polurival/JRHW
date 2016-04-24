71125692993d24af24df22333e7366e916b69749
Polurival <polurival@gmail.com>
2016-04-24 16:41:47 +0300

    right solution for level36.lesson04.big01 Task03 - MVC

--- src/com/javarush/test/level36/lesson04/big01/controller/Controller.java
+++ src/com/javarush/test/level36/lesson04/big01/controller/Controller.java
@@ -1,6 +1,7 @@
 package com.javarush.test.level36.lesson04.big01.controller;
 
 import com.javarush.test.level36.lesson04.big01.model.Model;
+import com.javarush.test.level36.lesson04.big01.view.UsersView;
 
 /**
  * Created by
@@ -9,6 +10,7 @@ import com.javarush.test.level36.lesson04.big01.model.Model;
 public class Controller
 {
     private Model model;
+    private UsersView usersView;
 
     public void setModel(Model model)
     {
@@ -18,5 +20,11 @@ public class Controller
     public void onShowAllUsers()
     {
         model.loadUsers();
+        usersView.refresh(model.getModelData());
+    }
+
+    public void setUsersView(UsersView usersView)
+    {
+        this.usersView = usersView;
     }
 }
---
+++ src/com/javarush/test/level36/lesson04/big01/view/UsersView.java
@@ -0,0 +1,36 @@
+package com.javarush.test.level36.lesson04.big01.view;
+
+import com.javarush.test.level36.lesson04.big01.bean.User;
+import com.javarush.test.level36.lesson04.big01.controller.Controller;
+import com.javarush.test.level36.lesson04.big01.model.ModelData;
+
+/**
+ * Created by
+ * Polurival on 24.04.2016.
+ */
+public class UsersView implements View
+{
+    private Controller controller;
+
+    @Override
+    public void refresh(ModelData modelData)
+    {
+        System.out.println("All users:");
+        for (User user : modelData.getUsers())
+        {
+            System.out.println("\t" + user);
+        }
+        System.out.println("===================================================");
+    }
+
+    @Override
+    public void setController(Controller controller)
+    {
+        this.controller = controller;
+    }
+
+    public void fireEventShowAllUsers()
+    {
+        controller.onShowAllUsers();
+    }
+}
