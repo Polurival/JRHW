ad340af82c5aa3857c5c7e1bb449ef06441f1b64
Polurival <polurival@gmail.com>
2016-04-27 21:15:05 +0300

    right solution for level37.lesson10.big01 Task01

---
+++ src/com/javarush/test/level37/lesson10/big01/AmigoSet.java
@@ -0,0 +1,23 @@
+package com.javarush.test.level37.lesson10.big01;
+
+import java.io.Serializable;
+import java.util.*;
+
+/**
+ * Created by
+ * Polurival on 27.04.2016.
+ */
+public class AmigoSet<E> extends AbstractSet implements Serializable, Cloneable, Set
+{
+    @Override
+    public Iterator<E> iterator()
+    {
+        return null;
+    }
+
+    @Override
+    public int size()
+    {
+        return 0;
+    }
+}
