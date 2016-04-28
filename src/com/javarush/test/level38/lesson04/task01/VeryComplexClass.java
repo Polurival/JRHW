222fe7a2a4bfaf2d2f198e2613d3d3a55273331c
Polurival <polurival@gmail.com>
2016-04-28 23:25:22 +0300

    right solution for level38.lesson04.task01 - Exceptions

---
+++ src/com/javarush/test/level38/lesson04/task01/VeryComplexClass.java
@@ -0,0 +1,15 @@
+package com.javarush.test.level38.lesson04.task01;
+
+/* Проверяемые исключения (checked exception)
+Напиши реализацию метода veryComplexMethod().
+Он должен всегда кидать какое-нибудь проверяемое исключение.
+Кинуть исключение (throw) явно нельзя.
+*/
+
+public class VeryComplexClass {
+    public void veryComplexMethod() throws Exception {
+        //напишите тут ваш код
+        String ex = "бла";
+        char c = ex.charAt(5);
+    }
+}
