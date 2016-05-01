cc46409bf65f6765a3d094667683da465e0015e9
Polurival <polurival@gmail.com>
2016-05-01 19:45:50 +0300

    right solution for level24.lesson14.big01 - Task03 - Arcanoid

--- src/com/javarush/test/level24/lesson14/big01/Arcanoid.java
+++ src/com/javarush/test/level24/lesson14/big01/Arcanoid.java
@@ -6,6 +6,35 @@ package com.javarush.test.level24.lesson14.big01;
  */
 public class Arcanoid
 {
+    private int width;
+    private int height;
+
+    public Arcanoid(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth()
+    {
+        return width;
+    }
+
+    public void setWidth(int width)
+    {
+        this.width = width;
+    }
+
+    public int getHeight()
+    {
+        return height;
+    }
+
+    public void setHeight(int height)
+    {
+        this.height = height;
+    }
+
     public static void main(String[] args)
     {
 
