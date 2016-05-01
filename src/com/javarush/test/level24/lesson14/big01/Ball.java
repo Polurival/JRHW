ec2be73dc033789873e6580e9cf885d4ffc20f81
Polurival <polurival@gmail.com>
2016-05-01 20:44:25 +0300

    right solution for level24.lesson14.big01 - Task14 - Arcanoid

--- src/com/javarush/test/level24/lesson14/big01/Ball.java
+++ src/com/javarush/test/level24/lesson14/big01/Ball.java
@@ -49,12 +49,21 @@ public class Ball extends BaseObject
     @Override
     public void draw(Canvas canvas)
     {
-
+        canvas.setPoint(x, y, 'O');
     }
 
     @Override
     public void move()
     {
+        if (!isFrozen)
+        {
+            x += dx;
+            y += dy;
+        }
+    }
 
+    public void start()
+    {
+        isFrozen = false;
     }
 }
