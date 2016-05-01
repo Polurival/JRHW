3ee7a16564e491c3fecf9034bded88462aff343a
Polurival <polurival@gmail.com>
2016-05-01 20:32:09 +0300

    right solution for level24.lesson14.big01 - Task11 - Arcanoid

--- src/com/javarush/test/level24/lesson14/big01/Canvas.java
+++ src/com/javarush/test/level24/lesson14/big01/Canvas.java
@@ -57,4 +57,29 @@ public class Canvas
             }
         }
     }
+
+    public void clear()
+    {
+        for (int i = 0; i < matrix.length; i++)
+        {
+            for (int j = 0; j < matrix[0].length; j++)
+            {
+                matrix[i][j] = ' ';
+            }
+        }
+    }
+
+    public void print()
+    {
+        for (int i = 0; i < matrix.length; i++)
+        {
+            for (int j = 0; j < matrix[0].length; j++)
+            {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+    }
 }
