8754c264a28ee13818d0bc202a86c1f6bd95dd3d
Polurival <polurival@gmail.com>
2016-05-01 20:41:14 +0300

    right solution for level24.lesson14.big01 - Task13 - Arcanoid

--- src/com/javarush/test/level24/lesson14/big01/Ball.java
+++ src/com/javarush/test/level24/lesson14/big01/Ball.java
@@ -4,11 +4,46 @@ package com.javarush.test.level24.lesson14.big01;
  * Created by
  * Polurival on 01.05.2016.
  */
+
 public class Ball extends BaseObject
 {
-    public Ball(double x, double y, double radius)
+    private double speed;
+    private double direction;
+    private double dx;
+    private double dy;
+    private boolean isFrozen;
+
+    public Ball(double x, double y, double speed, double direction)
     {
-        super(x, y, radius);
+        super(x, y, 1);
+        this.speed = speed;
+        this.direction = direction;
+        isFrozen = true;
+    }
+
+    public double getSpeed()
+    {
+        return speed;
+    }
+
+    public double getDirection()
+    {
+        return direction;
+    }
+
+    public double getDx()
+    {
+        return dx;
+    }
+
+    public double getDy()
+    {
+        return dy;
+    }
+
+    public boolean isFrozen()
+    {
+        return isFrozen;
     }
 
     @Override
