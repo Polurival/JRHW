fde02e5dd542b2fc41d99ddb3045cffd3978080d
Polurival <polurival@gmail.com>
2016-05-01 20:28:57 +0300

    right solution for level24.lesson14.big01 - Task10 - Arcanoid

--- src/com/javarush/test/level24/lesson14/big01/Canvas.java
+++ src/com/javarush/test/level24/lesson14/big01/Canvas.java
@@ -14,6 +14,7 @@ public class Canvas
     {
         this.width = width;
         this.height = height;
+        matrix = new char[height][width];
     }
 
     public int getWidth()
@@ -29,5 +30,31 @@ public class Canvas
     public char[][] getMatrix()
     {
         return matrix;
+    }
+
+    public void setPoint(double x, double y, char c)
+    {
+        int intX = (int) Math.round(x);
+        int intY = (int) Math.round(y);
+
+        //в) ничего не делать, если x<0 или y<0 или y>matrix.length или x>matrix[0].length
+        if (!(intX < 0 || intY < 0 || intY > matrix.length || intX > matrix[0].length))
+        {
+            matrix[intY][intX] = c;
+        }
+    }
+
+    public void drawMatrix(double x, double y, int[][] matrix, char c)
+    {
+        for (int i = 0; i < matrix.length; i++)
+        {
+            for (int j = 0; j < matrix[0].length; j++)
+            {
+                if (matrix[i][j] != 0)
+                {
+                    setPoint(x + j, y + i, c);
+                }
+            }
+        }
     }
 }
