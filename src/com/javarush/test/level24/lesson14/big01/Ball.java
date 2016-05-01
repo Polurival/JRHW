5809473e4518259792cef3417e1857340ce50037
Polurival <polurival@gmail.com>
2016-05-01 20:53:28 +0300

    right solution for level24.lesson14.big01 - Task16 - Arcanoid

--- src/com/javarush/test/level24/lesson14/big01/Ball.java
+++ src/com/javarush/test/level24/lesson14/big01/Ball.java
@@ -1,29 +1,40 @@
 package com.javarush.test.level24.lesson14.big01;
 
 /**
- * Created by
- * Polurival on 01.05.2016.
+ * Класс для шарика в игре
  */
-
 public class Ball extends BaseObject
 {
+    //скорость
     private double speed;
+    //направление  (в градусах от 0 до 360)
     private double direction;
+
+    //текущее значение вектора движения (dx,dy)
     private double dx;
     private double dy;
+
+    //заморожен ли объект или может двигаться
     private boolean isFrozen;
 
     public Ball(double x, double y, double speed, double direction)
     {
         super(x, y, 1);
-        this.speed = speed;
+
         this.direction = direction;
-        isFrozen = true;
+        this.speed = speed;
+
+        this.isFrozen = true;
     }
 
     public double getSpeed()
     {
         return speed;
+    }
+
+    public void setSpeed(double speed)
+    {
+        this.speed = speed;
     }
 
     public double getDirection()
@@ -41,42 +52,81 @@ public class Ball extends BaseObject
         return dy;
     }
 
-    public boolean isFrozen()
+    /**
+     * Устанавливаем новое направление движения.
+     * Тут же вычисляем и новый вектор.
+     * Тако подход удобно использовать при отскоках от стен.
+     */
+    public void setDirection(double direction)
     {
-        return isFrozen;
+        this.direction = direction;
+
+        double angel = Math.toRadians(direction);
+        dx = Math.cos(angel) * speed;
+        dy = -Math.sin(angel) * speed;
     }
 
+    /**
+     * Рисуем себя на "канвасе".
+     */
     @Override
     public void draw(Canvas canvas)
     {
         canvas.setPoint(x, y, 'O');
     }
 
-    @Override
+    /**
+     * Двигаем себя на один шаг.
+     */
     public void move()
     {
-        if (!isFrozen)
+        if (isFrozen) return;
+
+        x += dx;
+        y += dy;
+
+        checkRebound(1, Arcanoid.game.getWidth(), 1, Arcanoid.game.getHeight() + 5);
+    }
+
+    /**
+     * Проверяем не улетел ли шарик за стенку.
+     * Если да - отражаем его.
+     */
+    public void checkRebound(int minx, int maxx, int miny, int maxy)
+    {
+        if (x < minx)
         {
-            x += dx;
-            y += dy;
+            x = minx + (minx - x);
+            dx = -dx;
+        }
+
+        if (x > maxx)
+        {
+            x = maxx - (x - maxx);
+            dx = -dx;
+        }
+
+        if (y < miny)
+        {
+            y = miny + (miny - y);
+            dy = -dy;
+        }
+
+        if (y > maxy)
+        {
+            y = maxy - (y - maxy);
+            dy = -dy;
         }
     }
 
+    /**
+     * Запускам шарик.
+     * isFrozen = false.
+     * Пересчитываем вектор движения (dx,dy).
+     */
     public void start()
     {
-        isFrozen = false;
-    }
-
-    public void setDirection(double direction)
-    {
-        this.direction = direction;
-        double angel = Math.toRadians(direction);
-        dx = Math.cos(angel) * speed;
-        dy = -Math.sin(angel) * speed;
-    }
-
-    public void checkRebound(int minx, int maxx, int miny, int maxy)
-    {
-
+        this.setDirection(direction);
+        this.isFrozen = false;
     }
 }
--- src/com/javarush/test/level24/lesson14/big01/Stand.java
+++ src/com/javarush/test/level24/lesson14/big01/Stand.java
@@ -6,9 +6,24 @@ package com.javarush.test.level24.lesson14.big01;
  */
 public class Stand extends BaseObject
 {
-    public Stand(double x, double y, double radius)
+    private double speed;
+    private double direction;
+
+    public Stand(double x, double y)
     {
-        super(x, y, radius);
+        super(x, y, 3);
+        speed = 1;
+        direction = 0;
+    }
+
+    public double getSpeed()
+    {
+        return speed;
+    }
+
+    public double getDirection()
+    {
+        return direction;
     }
 
     @Override
