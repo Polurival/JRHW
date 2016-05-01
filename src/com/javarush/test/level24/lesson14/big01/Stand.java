1bea53d927e7f581b55cc466f5b8480438c7a29f
Polurival <polurival@gmail.com>
2016-05-01 21:10:44 +0300

    right solution for level24.lesson14.big01 - Task18 - Arcanoid

--- src/com/javarush/test/level24/lesson14/big01/Stand.java
+++ src/com/javarush/test/level24/lesson14/big01/Stand.java
@@ -1,19 +1,54 @@
 package com.javarush.test.level24.lesson14.big01;
 
 /**
- * Created by
- * Polurival on 01.05.2016.
+ *  Подставка, с помощью которой отражаем мячик.
  */
 public class Stand extends BaseObject
 {
-    private double speed;
-    private double direction;
+    //картинка для отрисовки
+    private static int[][] matrix = {
+            {1, 1, 1, 1, 1},
+            {1, 0, 0, 0, 1},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+    };
+
+    //скорость
+    private double speed = 1;
+    //направление (-1 влево, +1 вправо)
+    private double direction = 0;
 
     public Stand(double x, double y)
     {
-        super(x, y, 3);
-        speed = 1;
-        direction = 0;
+        super(x,y,3);
+    }
+
+    /**
+     * Метод передвигает подставку в соответствии с текущим значением direction.
+     */
+    public void move()
+    {
+        double dx = speed * direction;
+        x = x + dx;
+
+        checkBorders(radius, Arcanoid.game.getWidth() - radius + 1, 1, Arcanoid.game.getHeight() + 1);
+    }
+
+    /**
+     * direction устанавливается равным -1
+     */
+    public void moveLeft()
+    {
+        direction = -1;
+    }
+
+    /**
+     * direction устанавливается равным +1
+     */
+    public void moveRight()
+    {
+        direction = 1;
     }
 
     public double getSpeed()
@@ -26,25 +61,12 @@ public class Stand extends BaseObject
         return direction;
     }
 
+    /**
+     * Отрисовываем себя на холсте
+     */
     @Override
     public void draw(Canvas canvas)
     {
-
-    }
-
-    @Override
-    public void move()
-    {
-        x += direction * speed;
-    }
-
-    public void moveLeft()
-    {
-        direction = -1;
-    }
-
-    public void moveRight()
-    {
-        direction = 1;
+        canvas.drawMatrix(x - radius + 1, y, matrix, 'M');
     }
 }
